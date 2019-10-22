eb5ed43d0c70ef35a4c1b8e29d31b12ca25892b0
Cameron Sumpter <cameron@sumpter.xyz>
2019-10-22 12:50:13 -0400

    Comment dev code

--- src/roomkey/dryline/resource_specification_version.clj
+++ src/roomkey/dryline/resource_specification_version.clj
@@ -3,15 +3,15 @@
             [clojure.spec.alpha :as s]
             [clojure.string :as string]))
 
-(defn read-json-file [path]
-  (slurp path))
-
-(def aws-spec (json/parse-string (read-json-file "resources/aws/us-east-spec.json")
-                                                (fn [k] (-> k
-                                                            (string/replace #"(.*)::" "$1/")
-                                                            (string/replace #"::" ".")
-                                                            keyword))))
-
 (s/def ::ResourceSpecificationVersion #(re-matches #"^([\d]+[.]?)+$" %))
 
-(s/valid? ::ResourceSpecificationVersion (:ResourceSpecificationVersion aws-spec))
+(comment
+  ;; used for development - remove before release
+  (defn read-json-file [path]
+    (slurp path))
+  (def aws-spec (json/parse-string (read-json-file "resources/aws/us-east-spec.json")
+                                   (fn [k] (-> k
+                                               (string/replace #"(.*)::" "$1/")
+                                               (string/replace #"::" ".")
+                                               keyword))))
+  (s/valid? ::ResourceSpecificationVersion (:ResourceSpecificationVersion aws-spec)))
