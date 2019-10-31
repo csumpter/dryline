97892d6f07ba83154a5c8badcdecfac923967a0b
Sam Hewitt <shewitt@roomkey.com>
2019-10-31 16:53:35 -0400

    added test runner and a test for validating a cf specification

---
+++ test/roomkey/dryline/validation_test.clj
@@ -0,0 +1,12 @@
+(ns roomkey.dryline.validation-test
+  (:require [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
+            [clojure.test :as t]
+            [roomkey.dryline.parse :as parse]
+            [roomkey.dryline.validation]))
+
+(def parsed-spec (parse/parse (io/reader (io/resource "aws/us-east-spec.json"))))
+
+(t/deftest ^:unit valid-specification
+  (t/is (s/valid? :roomkey.aws.cloudformation/Specification parsed-spec)))
+
