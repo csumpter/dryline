e10a0f2d2180f221faa007a6f9a63ffea1a24840
Sam Hewitt <shewitt@roomkey.com>
2019-12-16 22:29:07 -0500

    add tests for correct spec generation

--- test/roomkey/dryline/spec_test.clj
+++ test/roomkey/dryline/spec_test.clj
@@ -28,9 +28,55 @@
 
 (t/deftest ^:unit spec-reference
   (let [sut #'specs/spec-reference]
-    (t/is (= (sut "AWS::ManagedBlockchain::Member.VotingPolicy"
-                  "ApprovalThresholdPolicy")
-             :roomkey.aws.managedblockchain.Member/ApprovalThresholdPolicy))
-    (t/is (= (sut "AWS::PinpointEmail::ConfigurationSet"
+    (t/is (= (sut "AWS::S3::Bucket.ObjectLockConfiguration"
+                  "Rule")
+             :roomkey.aws.s3.Bucket.Rule/Rule))
+    (t/is (= (sut "AWS::S3::Bucket"
                   "Tag")
              :roomkey.aws/Tag))))
+
+(t/deftest ^:unit s3-specs
+  (let [parsed-spec (-> "aws/S3BucketSpecification.json"
+                        io/resource
+                        io/reader
+                        parse/parse)]
+    (specs/gen-specs parsed-spec specs/primitive-type->predicate)
+    (t/is (= (s/describe :roomkey.aws.s3/Bucket)
+             '(keys
+               :opt-un
+               (:roomkey.aws.s3.Bucket/WebsiteConfiguration
+                :roomkey.aws.s3.Bucket/BucketEncryption
+                :roomkey.aws.s3.Bucket/BucketName
+                :roomkey.aws.s3.Bucket/LoggingConfiguration
+                :roomkey.aws.s3.Bucket/ReplicationConfiguration
+                :roomkey.aws.s3.Bucket/LifecycleConfiguration
+                :roomkey.aws.s3.Bucket/ObjectLockConfiguration
+                :roomkey.aws.s3.Bucket/CorsConfiguration
+                :roomkey.aws.s3.Bucket/InventoryConfigurations
+                :roomkey.aws.s3.Bucket/PublicAccessBlockConfiguration
+                :roomkey.aws.s3.Bucket/MetricsConfigurations
+                :roomkey.aws.s3.Bucket/NotificationConfiguration
+                :roomkey.aws.s3.Bucket/ObjectLockEnabled
+                :roomkey.aws.s3.Bucket/Tags
+                :roomkey.aws.s3.Bucket/VersioningConfiguration
+                :roomkey.aws.s3.Bucket/AccelerateConfiguration
+                :roomkey.aws.s3.Bucket/AnalyticsConfigurations
+                :roomkey.aws.s3.Bucket/AccessControl))))
+    (t/is (= (get (s/registry) :roomkey.aws.s3.Bucket/ObjectLockConfiguration)
+             :roomkey.aws.s3.Bucket.ObjectLockConfiguration/ObjectLockConfiguration))
+    (t/is (= (s/describe :roomkey.aws.s3.Bucket.ObjectLockConfiguration/ObjectLockConfiguration)
+             '(keys
+               :opt-un
+               (:roomkey.aws.s3.Bucket.ObjectLockConfiguration/Rule
+                :roomkey.aws.s3.Bucket.ObjectLockConfiguration/ObjectLockEnabled))))
+    (t/is (= (get (s/registry) :roomkey.aws.s3.Bucket.ObjectLockConfiguration/Rule)
+             :roomkey.aws.s3.Bucket.ObjectLockRule/ObjectLockRule))))
+
+
+(t/deftest ^:unit full-specs
+  (let [parsed-spec (-> "aws/us-east-spec.json"
+                        io/resource
+                        io/reader
+                        parse/parse)]
+    (t/is (= (specs/gen-specs parsed-spec specs/primitive-type->predicate)
+             nil))))
