9096200e17d5f371aae0765e7fc2a6dac170a4c8
Cameron Sumpter <csumpter@roomkey.com>
2020-03-06 14:30:36 -0500

    Refactoring work with Sam

--- src/roomkey/dryline/documentation.clj
+++ src/roomkey/dryline/documentation.clj
@@ -1,11 +1,10 @@
-(ns roomkey.dryline.documentation
+(ns roomkey.dryline.documentation ; rename
   (:require [hickory.core :as hick]
             [hickory.select :as hick.sel]
             [clojure.java.io]
             [clojure.string]
             [markdown.core]
             [roomkey.dryline.util :refer [dryline-keyword]]
-            [clojure.walk :refer [postwalk]]
             [clojure.pprint]))
 
 (defn- load-as-hickory [md-file-path]
@@ -21,21 +20,19 @@
    hick-data))
 
 (defn- get-property [property hick-data]
-  (remove nil? (map (fn [{:keys [content] :as original}]
-                      (let [{:keys [content]} (first content)]
-                        (when (= (first content) property)
-                          original)))
-                    hick-data)))
+  (->> hick-data
+       (map (fn [{:keys [content] :as original}]
+              (let [{:keys [content]} (first content)]
+                (when (= (first content) property)
+                  original))))
+       (remove nil?)))
 
 (defn- get-in-hickory [hick-data attr]
-  (try
-    (let [value (-> (drop-while (fn [{:keys [content]}]
-                                      (not (= attr (first content)))) (:content (first hick-data)))
-                        (nth 2)
-                        :content
-                        first)]
-      value)
-    (catch Exception e nil)))
+  (-> (drop-while (fn [{:keys [content]}]
+                    (not (= attr (first content)))) (:content (first hick-data)))
+      (nth 2 nil)
+      :content
+      first))
 
 (defmacro get-value [hick-data value & options]
   `(try
@@ -44,78 +41,60 @@
      (catch Exception e# nil)))
 
 (defn- get-title [hick-data]
-  (try
-    (let [h1-data (get-tag hick-data :h1)
-          raw-title (-> (first h1-data)
-                        :content
-                        first)]
-      (if (> (count raw-title) 1)
-        (dryline-keyword (clojure.string/join "." (clojure.string/split raw-title #"\s")))
-        (dryline-keyword raw-title)))
-    (catch Exception e nil)))
+  (let [h1-data (get-tag hick-data :h1)
+        raw-title (-> (first h1-data)
+                      :content
+                      first)]
+    (case raw-title
+      "Resource Tag" :roomkey.aws/Tag
+      (dryline-keyword (clojure.string/join "." (clojure.string/split raw-title #"\s"))))))
 
 (defn- get-properties [hick-data]
-  (remove nil? (map (fn [{:keys [content]}]
-                      (when (vector? content)
-                        (-> content
-                            first
-                            :content
-                            first)))
-                    hick-data)))
+  (->> hick-data
+       (map (fn [{:keys [content]}]
+              (some-> content
+                      first
+                      :content
+                      first)))
+       (remove nil?)))
 
-(defn- remove-nils
-  [m]
-  (let [f (fn [[k v]] (when v [k v]))]
-    (postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
-
-(defn- ?assoc
-  "Same as assoc, but skip the assoc if v is nil"
-  [m & kvs]
-  (->> kvs
-       (partition 2)
-       (filter second)
-       (map vec)
-       (into m)))
+(defn- property-values [p-elements property-name]
+  (let [prop-hick (get-property property-name p-elements)
+                                 regex (get-value prop-hick "Pattern" re-pattern)
+                                 enumerations (when-let [rtn (get-value prop-hick "Allowed Values")]
+                                                (-> (clojure.string/split rtn #"\s\|\s")
+                                                    set))
+                                 minimum (get-value prop-hick "Minimum" Integer/parseInt)
+                                 maximum (get-value prop-hick "Maximum" Integer/parseInt)
+                                 property-values (cond-> {}
+                                                  regex (assoc :regex regex)
+                                                  enumerations (assoc :enumerations enumerations)
+                                                  minimum (assoc :minimum minimum)
+                                                  maximum (assoc :maximum maximum))]
+                             [(keyword property-name) property-values])) ;factor out
 
 (defn- scrape-file [path]
   (let [file (load-as-hickory path)
         p-elements (get-tag file :p)
         properties (get-properties p-elements)
         title (get-title file)]
-    {(keyword title)
-     (apply merge
-            (map (fn [property-name]
-                   (let [prop-hick (get-property property-name p-elements)
-                         regex (get-value prop-hick "Pattern" re-pattern)
-                         enumerations (when-let [rtn (get-value prop-hick "Allowed Values")]
-                                        (-> (clojure.string/split rtn #"\s\|\s")
-                                            set))
-                         minimum (get-value prop-hick "Minimum" Integer/parseInt)
-                         maximum (get-value prop-hick "Maximum" Integer/parseInt)]
-                     (when (or regex enumerations minimum maximum)
-                       {(keyword property-name)
-                        (?assoc {}
-                                :regex regex
-                                :enumerations enumerations
-                                :minimum minimum
-                                :maximum maximum)})))
-                 properties))}))
+    [(keyword title)
+     (into {} (comp (map (partial property-values p-elements))
+                    (filter (comp seq second)))
+           properties)]))
 
 (defn scrape-property-specs [path-to-repo]
-  (let [property-files (remove nil?
-                               (map
-                                (fn [path] (when (re-matches #"^aws-properties.*" path) path))
-                                (->> "doc_source/"
-                                     (str path-to-repo)
-                                     clojure.java.io/file
-                                     .list
-                                     seq)))
-        shaped-prop-files (map #(str path-to-repo "doc_source/" %) property-files)
-        scraped-data (->> shaped-prop-files
-                          (map scrape-file)
-                          (apply merge)
-                          remove-nils)]
-    scraped-data))
+  (let [property-files (->> (->> "doc_source/"
+                                 (str path-to-repo)
+                                 clojure.java.io/file ;get absolute path
+                                 .list
+                                 seq)
+                            (filter (fn [path] (when (re-matches #"^aws-properties.*" path) path)))
+                            (remove #{"aws-properties-name.md"}))
+        shaped-prop-files (map #(str path-to-repo "doc_source/" %) property-files)]
+    (into {} (comp (map (partial scrape-file))
+                   (filter (comp seq second)))
+              shaped-prop-files)))
 
 (comment
 
@@ -124,6 +103,11 @@
 (clojure.pprint/pprint
  (scrape-property-specs ".../aws-cloudformation-user-guide/")
  (clojure.java.io/writer "resources/scraped-spec-data.edn"))
+
+(def a (load-as-hickory "/Users/csumpter/code/aws-cloudformation-user-guide/doc_source/aws-properties-cw-alarm.md"))
+
+(def b (get-properties a))
+
 
 )
 
